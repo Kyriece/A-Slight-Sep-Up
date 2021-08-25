390f43754d34c2a2dde167dddbbdb78c2b6cad10
nannair <nandinivin@gmail.com>
2021-08-25 17:54:47 +1000

    dashboard edited to display categories (no backend)

--- SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Dashboard.js
+++ SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Dashboard.js
@@ -9,15 +9,19 @@ class Dashboard extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <h1 className="display-4 text-center">Persons</h1>
-                        <br />
-                       <CreatePersonButton />
-                        <br />
-                        <hr />
+                        <h1 className="display-4 text-center">Category 1</h1>
                         <Person/>
                     </div>
                 </div>
             </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Category 2</h1>
+                            <Person/>
+                        </div>
+                    </div>
+                </div>
         </div>
     
         )
