4cf6397305751c015e7fb485b2138b476c9aeb62
spacediscotqtt <79812132+spacediscotqtt@users.noreply.github.com>
2021-10-20 12:45:44 +1100

    request backend wip

---
+++ SEPT2021_MajorProject/BackEnd/requestsmicroservice/src/main/java/com/rmit/sept/bk_requests/model/Requests.java
@@ -0,0 +1,44 @@
+package com.rmit.sept.bk_requests.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "requests")
+public class Requests {
+    @Id
+	@GeneratedValue
+	private Long id;
+
+    @Column(nullable = false)
+	private String user;
+
+    @Column(nullable = false)
+	private String requestComment;
+
+    public void setId(Long id) {
+		this.id = id;
+    }
+
+    public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+    public String getRequestComment() {
+		return requestComment;
+	}
+
+	public void setrequestComment(String requestComment) {
+		this.requestComment = requestComment;
+	}
+
+
+
+}
