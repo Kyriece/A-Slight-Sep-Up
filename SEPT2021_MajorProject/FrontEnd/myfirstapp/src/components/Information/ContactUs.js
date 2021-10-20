b64447df6bcbe5cf5906cafac4130a385ff4defa
vi-ca <s3851213@student.rmit.edu.au>
2021-10-20 13:13:59 +1100

    creating a contact us box

--- SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/ContactUs.js
+++ SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/ContactUs.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Person from '../Persons/Person'
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 import { Link } from 'react-router-dom';
 import store from "../../store";
+import './ContactUs.css';
 
 
 class ContactUs extends Component {
 
     render() {
+
+      const handleSubmit = event => {
+        event.preventDefault();
+        alert('You have submitted the form.')
+      }
         const user = store.getState().security.user
         console.log(user);
         return (
@@ -25,6 +31,15 @@ class ContactUs extends Component {
                                     <hr />
                                     <p>If you have any issues, send through an email to the address below. Please expect a 24-48 hour reply period. Thank you for your patience.</p>
                                     <p>support@bookaroo.com.au</p>
+                                    <p> alternatively, submit a request below! </p>
+                                    <form onSubmit={handleSubmit}>
+                                      <fieldset>  
+                                        <input className="formBox" type="text" name="name" placeholder="Request Title"/>
+                                        <input className="formBox" type="text" name="info" placeholder="Request Information"/>
+                                        <input className="formBox" type="text" name="email" placeholder="Email"/>
+                                      </fieldset>
+                                      <button type="submit">Submit Request</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
