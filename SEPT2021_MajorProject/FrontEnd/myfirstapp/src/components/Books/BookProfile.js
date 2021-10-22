c74ae2dd6c694e7a2d53e7b464b05117083bcbb7
nannair <nandinivin@gmail.com>
2021-10-23 01:24:01 +1100

    made the book profile page pretty

--- SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Books/BookProfile.js
+++ SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Books/BookProfile.js
@@ -17,7 +17,7 @@ class BookProfile extends Component{
         super();
         this.state = this.initialState;
         console.log(this.state)
-        
+
     }
 
     initialState = {
@@ -58,16 +58,16 @@ class BookProfile extends Component{
 
         render() {
             console.log(this.state)
-           
-           
+
+
             // this.props.fetchBook(id);
             // let book = this.props.book;
             // console.log(book);
             return (
-                
+
                 <>
                 <Header />
-                <div class="container profile" style={{paddingBottom: '400px'}}>
+                <div class="col-md-6 border-end" >
                     <div class="column" >
                         <div class="col-md-4">
                             <div style={{ paddingRight: '50px', float: 'left', width: '300px' }}>
@@ -75,46 +75,71 @@ class BookProfile extends Component{
                             </div>
                         </div>
                     </div>
-                    <div style={{ paddingLeft: '20px', float:'left', width:'790px', height:'100%' }}>
-                        <br></br>
-                        <h1 className="display-4 text-center"> {this.state.title}</h1>
-                        <div></div>
-                        <p1 className="display-8 text-center" >{this.state.blurb}</p1>
-                    </div>
                 </div>
+
+<div className="col-md-6">
+    <div className="p-3 right-side">
+                    <div className= "d-flex justify-content-between align-items-center">
+                        <h3 className="display-4 text-center"> {this.state.title} blah blah</h3>
+                    </div>
+
+                        <div className="mt-2 pr-3 content">
+                        <p1>{this.state.blurb} blah blah blah</p1>
+                        </div>
+        <div>
+            <h6 style={{ float:"left", width: '790px'}}> Genre: {this.state.genre}<br /> Language: {this.state.language}<br /> Author: {this.state.author} </h6>
+        </div>
+
+
+
+
                 <div class="container profile" >
                     <div class = "column">
-                        <div class="col-md-4" style={{ float:'left', width: '300px'}}>
-                            <h3 >Rating: {this.state.rating}/5</h3>
-                            <div className="btn btn-lg btn-secondary mr-2">
+                        <div class="ratings d-flex flex-row align-items-center">
+                            <h6>Rating: {this.state.rating}/5</h6>
+                        </div>
+                        <div className="btn btn-lg btn-primary mr-2">
                             <PayPalButtons style={{ layout: "horizontal" }} />
                             Buy Now: ${this.state.price}
+                        </div>
+                    </div>
+                </div>
+        <br></br>
+        <div className="row row-cols-1 row-cols-md-3">
+            <div className="col mb-4">
+        <div className="card" style={{width: '18rem'}}>
+            <h3 class="card-header" > Table of contents <br></br>  </h3>
+            <p>{this.state.tableOfContents}</p>
+        </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+        <hr />
+
+
+
+                <div class="col-sm-6">
+                    <div class = "card">
+
+                            <h3 class="card-header">About the Author</h3>
+                            <div className="card-body">
+                            <p>{this.state.authorDescription}</p>
                             </div>
-                        </div>
-                    </div>
-                    <div> 
-                        <h3 style={{ float:"left", width: '790px'}}> Genre: {this.state.genre}<br /> Language: {this.state.language}<br /> Author: {this.state.author} </h3>
-                    </div>
-                </div>
-                <br /><br />
-                <div class="container profile">
-                    <div class = "column">
-                        <div class="col-md-4" style={{ float:'left', width: '300px', paddingRight: "30px"}}> 
-                            <h3>About the Author</h3>
-                            <p >{this.state.authorDescription}</p>
-                        </div>
-                    </div>
-                    <div style={{ float:"left", width: '600px'}}>
-                    <h3> Table of contents <br></br>  </h3>
-                    <p>{this.state.tableOfContents}</p>
-                    </div>
-                </div>
+
+                    </div></div>
+
+
+
+
             </>
         );
     }
 }
 
-  
+
 
 
 export default BookProfile
