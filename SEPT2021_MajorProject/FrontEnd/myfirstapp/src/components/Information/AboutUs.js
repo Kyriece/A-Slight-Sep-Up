4f8d6fa5ba47225a9fc52f487997607da0915cf6
vi-ca <s3851213@student.rmit.edu.au>
2021-10-24 16:10:45 +1100

    re-formatting + commenting

--- SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/AboutUs.js
+++ SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/AboutUs.js
@@ -1,39 +1,32 @@
 import React, { Component } from 'react'
-import Person from '../Persons/Person'
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
-import { Link } from 'react-router-dom';
-import store from "../../store";
 
-
+// simple webpage for information section
 class AboutUs extends Component {
-
-    render() {
-        const user = store.getState().security.user
-        console.log(user);
-        return (
-            <>
-                <Header />
-                <div className="landing">
-                    <div className="light-overlay landing-inner text-dark">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-md-12 text-center">
-                                    <h1 className="display-8 mb-4">
-                                        About Us
-                                    </h1>
-                                    <hr />
-                                    <p>A small team of developers with a love for books and an exciting dream!</p>
-                                    <p>Bookeroo wouldn't be possible if it wasn't backed by our lovely membership base, thank you for all your support!</p>
-                                </div>
+  render() {
+    return (
+        <>
+          <Header />
+          {/* the header follows some general bootstrap styling */}
+            <div className="landing">
+                <div className="light-overlay landing-inner text-dark">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 text-center">
+                                <h1 className="display-8 mb-4"> About Us </h1>
+                                <hr />
+                                {/* hard coded information statement */}
+                                <p>A small team of developers with a love for books and an exciting dream!</p>
+                                <p>Bookeroo wouldn't be possible if it wasn't backed by our lovely membership base, thank you for all your support!</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <Footer />
-            </>
-    
-        )
-    }
+            </div>
+          <Footer />
+        </>
+    )
+  }
 }
 export default AboutUs;
--- SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/AdminRequests.js
+++ SEPT2021_MajorProject/FrontEnd/myfirstapp/src/components/Information/AdminRequests.js
@@ -8,61 +8,63 @@ import Footer from "./Layout/Footer";
 
 class UserManager extends Component {
 
-    componentDidMount() {
-        this.props.getUsers();
-    }
+  componentDidMount() {
+      this.props.getUsers();
+  }
 
-    getDataItems(data) {
-        return data.map((dataItem)=>(
-            <tr key={dataItem.id}>
-                <td>{dataItem.id}</td>
-                <td>{dataItem.ti}</td>
-                <td>{dataItem.fullName}</td>
-                <td>{dataItem.userStatus}</td>
-                <td>{dataItem.publisherrequest ? <div>TRUE</div> : <div>FALSE</div>}</td>
+  getDataItems(data) {
+    return data.map((dataItem)=>(
+      <tr key={dataItem.id}>
+        <td>{dataItem.id}</td>
+        <td>{dataItem.ti}</td>
+        <td>{dataItem.fullName}</td>
+        <td>{dataItem.userStatus}</td>
+        <td>{dataItem.publisherrequest ? <div>TRUE</div> : <div>FALSE</div>}</td>
+      </tr>
+    ))
+  }
+
+render() {
+  const {users} = this.props.user;
+  let userList = [];
+  // react loop to iterate all the users into the userList array
+  for (let value of users) {
+    userList.push(value);
+  }
+  const userstable = this.getDataItems(userList)
+
+  return (
+    <>
+    {/* header variable declared on every page, as well as the footer */}
+      <Header/>
+      <div>
+        <h1 className="display-4 text-center"> User Manager Page </h1>
+        <br/>
+        <br/>
+        {/* bootstrap table styling and formatting */}
+        <table className = "table table-striped">
+          <thead>
+            <tr>
+              {/* table values defined */}
+              <td>id</td>
+              <td>username</td>
+              <td>fullName</td>
+              <td>userstatus</td>
+              <td>publisherrequest</td>
             </tr>
-        ))
-    }
-
-    render() {
-
-        const {users} = this.props.user;
-        console.log(users);
-        let userList = [];
-        for (let i =0; i < users.length; i++) {
-            userList.push(users[i]);
-        }
-        const userstable = this.getDataItems(userList)
-
-        return (
-            <>
-            <Header/>
-            <div>
-                <h1 className="display-4 text-center"> User Manager Page </h1>
-                <br/>
-                <br/>
-                <table className = "table table-striped">
-                    <thead>
-                        <tr>
-                            <td>id</td>
-                            <td>username</td>
-                            <td>fullName</td>
-                            <td>userstatus</td>
-                            <td>publisherrequest</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {userstable}
-                    </tbody>
-                </table>
-            </div>
-            <Footer />
-
-            </>
-        );
-    }
+          </thead>
+            <tbody>
+              {userstable}
+            </tbody>
+          </table>
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
 
+// render and return the compiled application
 UserManager.propTypes ={
     user: PropTypes.object.isRequired,
     getUsers: PropTypes.func.isRequired
